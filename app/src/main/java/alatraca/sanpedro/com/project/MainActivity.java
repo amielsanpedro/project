18d2fc0a5c90ba4ca4e6ede087d1443616b49ced
amielsanpedro <amieljustin.sanpedro.iics@ust.edu.ph>
2018-11-12 23:36:01 +0800

    Initial commit

---
+++ app/src/main/java/alatraca/sanpedro/com/project/MainActivity.java
@@ -0,0 +1,33 @@
+package alatraca.sanpedro.com.project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+
+    }
+}
